b92b7fa8cafaa7cb6124906165cfc11df843c82b
Sara F. Pereira <30632636+leevyth@users.noreply.github.com>
2018-06-19 13:52:28 +0200

    Update BotSavesPrincess.py

--- BotSavesPrincess.py
+++ BotSavesPrincess.py
@@ -2,6 +2,7 @@
 
 def displayPathtoPrincess(n,grid):
 #print all the moves here
+    print(grid)
     for i, j in enumerate(grid): 
         if 'p' in j:
             princess = (i, j.index('p'))
@@ -9,13 +10,13 @@ def displayPathtoPrincess(n,grid):
             mario = (i, j.index('m'))
     distance = (princess[0] - mario[0], princess[1] - mario[1])
     if distance[0] < 0:
-        print('LEFT' * abs(distance[0]))
+        print('LEFT\n' * abs(distance[0]), end ='')
     elif distance[0] > 0:
-        print('RIGHT' * abs(distance[0]))
+        print('RIGHT\n' * abs(distance[0]), end = '')
     if distance[1] < 0:
-        print('UP' * abs(distance[1]))
+        print('UP\n' * abs(distance[1]), end = '')
     elif distance[1] > 0:
-        print('DOWN' * abs(distance[1]))
+        print('DOWN\n' * abs(distance[1]), end = '')
             
             
 
